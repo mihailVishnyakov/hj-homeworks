cb0e7f69fd1c5bcdf5bed7185a7a0247b3e9cb05
mihailVishnyakov <alunos13@yandex.ru>
2020-05-10 10:08:50 +0300

    weather-widget final

--- xhr/weather-widget/js/main.js
+++ xhr/weather-widget/js/main.js
@@ -1,7 +1,12 @@
 const request = new XMLHttpRequest();
-request.open('GET', 'https://neto-api.herokuapp.com/weather', false);
+//меняем тип запроса на асинхронный
+request.open('GET', 'https://neto-api.herokuapp.com/weather', true);
 request.send();
-if (request.status === 200) {
-  const response = JSON.parse(request.responseText);
-  setData(response);
-}
+
+//проверяем XHR-запрос на окончание загрузки
+request.addEventListener('load',() => {
+	if (request.status === 200) {
+	  const response = JSON.parse(request.responseText);
+	  setData(response);
+	}
+})
